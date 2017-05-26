e84aa98ac52dffc48d3ba7b13b8c263c3340466f
ShanaSkydancer <shana.proverbs31@gmail.com>
2017-05-26 14:53:31 +0200

    Clean coding

--- addRegiNumbers.js
+++ addRegiNumbers.js
@@ -1,8 +1,9 @@
+//Adds each registration number in a list, but does not add an empty value
 function addRegiNumbers() {
   var li = document.createElement("li");
-    if (inputRegiNumbers.value !== ""){
-      li.setAttribute("id", "inputRegiNumbers");
-      ul.appendChild(li);
-      li.innerHTML = inputRegiNumbers.value;
-      }
+  if (inputRegiNumbers.value !== "") {
+    li.setAttribute("id", "inputRegiNumbers");
+    ul.appendChild(li);
+    li.innerHTML = inputRegiNumbers.value;
+  }
 }
--- main.js
+++ main.js
@@ -5,29 +5,23 @@ var regiListForRegis = document.querySelector("#regiOutputList");
 var ul = document.querySelector("ul");
 var li = document.createElement("li");
 var dropDownButt = document.querySelector(".ddButton");
-var ddValues = document.querySelector("#myDropdown");
-var dropdowns = document.querySelector("ddContent");
-var ddSection = document.getElementsByClassName("dropdown");
-var ouputTown = document.getElementById("selectTownOutput");
-var ddOptions = document.getElementsByClassName("ddOptions");
 
+//Listens for when you submit the form
 form.addEventListener("submit", function() {
   event.preventDefault();
   li.innerHTML = addRegiNumbers()
 });
 
-addRegiNumbers();
-
+//Displays the selected Town's registration numbers
 dropDownButt.addEventListener('change', function() {
   const currentOption = event.target;
   const currentOptionID = currentOption.value;
   var li = ul.getElementsByTagName('li');
-  for(var i = 0; i < li.length; i++){
+  for (var i = 0; i < li.length; i++) {
     var currentList = li[i].textContent;
-    if(currentOptionID === 'ALL' || currentList.startsWith(currentOptionID)){
+    if (currentOptionID === 'ALL' || currentList.startsWith(currentOptionID)) {
       li[i].style.display = 'block';
-    }
-    else{
+    } else {
       li[i].style.display = 'none';
     };
   };
